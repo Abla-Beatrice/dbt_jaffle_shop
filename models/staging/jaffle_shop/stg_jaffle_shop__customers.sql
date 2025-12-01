04e4d9d8eac7ba07bf3b8a880dc1ebd2ec51042d
Beatrice Segbegnon <asegbegnon1@student.gsu.edu>
2025-12-01 18:51:05 +0000

    commit updated files

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,7 +1,15 @@
-select 
-    id as customer_id,
-    first_name,
-    last_name
-from {{ source('jaffle_shop', 'customers') }}
+with 
 
-select * from renamed
+source as ( 
+
+select * from from {{ source('jaffle_shop', 'customers') }}
+),
+
+renamed as ( 
+    select 
+        id as customer_id,
+        first_name,
+        last_name
+
+from source
+)
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,8 +1,16 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-from {{ source('jaffle_shop', 'orders') }}
+with 
 
-select * from renamed
+source as ( 
+
+select * from from {{ source('jaffle_shop', 'orders') }}
+),
+
+renamed as ( 
+    select 
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status as order_status
+
+    from source
+)
